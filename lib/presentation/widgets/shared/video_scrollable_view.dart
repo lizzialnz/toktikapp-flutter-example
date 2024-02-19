3d4cbe6ace5ff4e01f896c1bbabc5151cef6ca8c
lizzialnz <lizzialnz@gmail.com>
2024-02-19 14:14:46 -0600

    botones, reproductor con pausa y gradiente.

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,7 +1,8 @@
-// reproductor de video scrollable
-
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
+// reproductor de video scrollable
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -9,12 +10,36 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.green),
-        Container(color: Colors.blue),
-      ],
+    // el page view es silimilar a un listado y permite hacer un scroll a pantalla completa
+    return PageView.builder(
+      // physics: const BouncingScrollPhysics(), // permite hacer un scroll con efecto de rebote en android
+      scrollDirection: Axis.vertical,
+      // cuantos elementos va a tener el page view
+      itemCount: videos.length,
+      // builder no recibe children sino itemBuilder para cargarlos de forma dinamica
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+
+        // se agregara un stack que permitira agregar un wiget encima de otros, en este caso el gradiente encima del video y encima de este los botones
+
+        return Stack(
+          children: [
+            // video player +gradientes
+            SizedBox.expand(
+                child: FullScreenPlayer(
+              caption: videoPost.caption,
+              videoUrl: videoPost.videoUrl,
+            )),
+
+            // botones
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost),
+            ),
+          ],
+        );
+      },
     );
   }
 }
